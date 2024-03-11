f9da25f582882a13426a18aa9a09299ca6d180aa
MandichRiccardo <ricky.mandich@gmail.com>
2024-03-11 17:08:13 +0100

    creazione 2024 03 11 17:08

---
+++ main.py
@@ -0,0 +1,27 @@
+# esercizio 1
+mia_lista = []
+print(mia_lista)
+# esercizio 2
+numeri = list(range(1, 6))
+print(numeri)
+# esercizio 3
+terzo_elemento = numeri[3]
+print(terzo_elemento)
+# esercizio 4
+numeri.append(6)
+print(numeri)
+# esercizio 5
+del numeri[3]
+print(numeri)
+# esercizio 6
+primi_tre = numeri[:3]
+print(primi_tre)
+# esercizio 7
+dispari = numeri[1::2]
+print(dispari)
+# esercizio 8
+numeri.sort(reverse=True)
+print(numeri)
+#esercizio 9
+count_2 = numeri.count(2)
+print(count_2)
